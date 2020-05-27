89e7380d8307deed77ba9d077d48eb305a1adf88
bartvanhoey <bartvanhoey@hotmail.com>
2020-05-27 09:32:50 +0200

    commit-2020-05-27- 932

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -61,7 +61,7 @@
       "url": "img\/blogpost001_folderstructure.png"
     },
     {
-      "hash": "sha256-TNPx+tts3LIDXao9VfiXxv5w0dO6pBoHTE3kZyZBl4Y=",
+      "hash": "sha256-dgu+YqDD7AwOXUOV26W7K2V7Ckk7MoEXq71h\/ZfS1ec=",
       "url": "index.html"
     },
     {
