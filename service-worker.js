5f2524af9942d5599ebc6f76711c35c8f529a1fe
bartvanhoey <bartvanhoey@hotmail.com>
2020-05-31 09:02:30 +0200

    commit-2020-05-31- 902

--- service-worker.js
+++ service-worker.js
@@ -14,12 +14,12 @@ const offlineAssetsExclude = [ /^service-worker\.js$/ ];
 async function onInstall(event) {
     console.info('Installing Service Worker');
 
-    // // Fetch and cache all matching items from the assets manifest
-    // const assetsRequests = self.assetsManifest.assets
-    //     .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
-    //     .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
-    //     .map(asset => new Request(asset.url, { integrity: asset.hash }));
-    // await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
+    // Fetch and cache all matching items from the assets manifest
+    const assetsRequests = self.assetsManifest.assets
+        .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
+        .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
+        .map(asset => new Request(asset.url, { integrity: asset.hash }));
+    await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
 }
 
 async function onActivate(event) {
